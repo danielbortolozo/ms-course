05246f7d2af72cd7f05d685849c02b823fd5de76
Daniel Rocca Bortolozo <danielbortolozo@hotmail.com>
2020-11-30 01:02:52 -0300

    hr-worker as a config server client

--- hr-worker/src/main/java/br/com/sisdb/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/br/com/sisdb/hrworker/resources/WorkerResource.java
@@ -5,6 +5,7 @@ import br.com.sisdb.hrworker.repositories.WorkerRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,6 +21,9 @@ public class WorkerResource {
 
     private static Logger logger =  LoggerFactory.getLogger(WorkerResource.class);
 
+    @Value("${test.config}")
+    private String testConfig;
+
     @Autowired
     private WorkerRepository repository;
     @Autowired
@@ -28,9 +32,16 @@ public class WorkerResource {
     @GetMapping
     public ResponseEntity<List<Worker>> findAll() {
         List<Worker> list = repository.findAll();
-
         return ResponseEntity.ok(list);
     }
+
+    @GetMapping(value = "/configs")
+    public ResponseEntity<Void> getConfigs() {
+        logger.info("Config :"+testConfig);
+        return ResponseEntity.noContent().build();
+    }
+
+
     @GetMapping("/{id}")
     public ResponseEntity<Worker> findById(@PathVariable Long id)  {
         Worker object = repository.findById(id).get();
