046fe399594751489e23dfdb89bf42f86b7f8ede
Daniel Rocca Bortolozo <danielbortolozo@hotmail.com>
2020-12-07 00:05:16 -0300

    Criando projeto hr-users

---
+++ hr-users/src/test/java/br/com/sisdb/hrusers/HrUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sisdb.hrusers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
